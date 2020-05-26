703cfcdb41f6150f5f168703e95f7028fea0c85d
Gal Azaria <galazariab@gmail.com>
2020-05-27 01:21:42 +0300

    Log error and Log event are in progress

--- src/main/java/com/example/demo/DomainLayer/Users/TeamManager.java
+++ src/main/java/com/example/demo/DomainLayer/Users/TeamManager.java
@@ -11,6 +11,9 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.*;
 
+import static com.example.demo.DemoApplication.errorLogger;
+import static com.example.demo.DemoApplication.eventLogger;
+
 @Entity
 public class TeamManager extends SystemUser implements Observer , Serializable {
 
@@ -71,45 +74,61 @@ public class TeamManager extends SystemUser implements Observer , Serializable {
         this.owner = owner;
     }
 
+
     public boolean addPlayers(Set<Integer> players){
         if(this.premissions.AddPlayerPermission() && owner != null ){
+            eventLogger.info("New Players were added to team: "+team.getName()+" ,by Team Manager: "+this.getSid());
             return owner.addPlayers(players);
         }
+        errorLogger.error("Team Manager: "+ this.getSid()+ " failed to add Players to Team: "+team.getName());
         return false;
     }
 
     public boolean removePlayer(int player){
         if( this.premissions.RemovePlayerPermission() && owner != null ){
+            eventLogger.info("New Players were removed from team: "+team.getName()+" ,by Team Manager: "+this.getSid());
+
             return owner.removePlayer(player);
         }
+        errorLogger.error("Team Manager: "+ this.getSid()+ " failed to remove Players from Team: "+team.getName());
         return false;
     }
 
     public boolean editPlayerRole(int player, PlayerRole role){
         if(this.premissions.EditPlayerRolePermission() && owner != null ){
+            eventLogger.info("A new Role: "+ role.name() +"set to Player: "+player+ " ,by: "+this.getSid());
             return owner.editPlayerRole(player,role);
         }
+        errorLogger.error("Team Manager: "+ this.getSid()+ " failed to edit Player role to Team: "+team.getName());
         return false;
     }
 
     public boolean addCoach(int coach_id) {
         if( premissions.AddCoachPermission() && owner != null ){
+            eventLogger.info("A new Coach: " + coach_id+ " was added to Team: "+this.getSid()+" ,by: "+this.getSid());
             return owner.addCoach(coach_id);
         }
+        errorLogger.error("Team Manager: "+ this.getSid()+ " failed to add owner to Team: "+team.getName());
         return false;
     }
 
     public boolean removeCoach(int coid) {
         if( premissions.RemoveCoachPermission() && owner != null){
+            eventLogger.info("A new Coach: " + coid+ " was removed from Team: "+this.getSid()+" ,by: "+this.getSid());
+
             return owner.removeCoach(coid);
         }
+        errorLogger.error("Team Manager: "+ this.getSid()+ " failed to remove Coach from Team: "+team.getName());
         return false;
     }
 
     public boolean editCoachRole(int coach, CoachRole role){
         if( premissions.EditCoachRolePermission() && owner != null){
+            eventLogger.info("A new Role: "+ role.name() +"set to Coach: "+coach+ " ,by: "+this.getSid());
             return owner.editCoachRole(coach,role);
         }
+        errorLogger.error("Team Manager: "+ this.getSid()+ " failed to edit Role to Coach: "+coach);
+
         return false;
     }
 
